418a63bd4231e386844c0edc0ccc8b08da9833fa
daniel njuguna <njugunadaniel364@gmail.com>
2019-09-30 18:02:51 +0300

    add an errorhandler to catch the error resulting from a wrong input format

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import datetime as dt
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
 
 # Create your views here.
 def welcome(request):
@@ -34,9 +34,15 @@ def convert_dates(dates):
 
 
 def past_days_news(request,past_date):
+    
+    try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+    
     day = convert_dates(date)
     html = f'''
         <html>
