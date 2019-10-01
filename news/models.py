1606820422b6dfa97e0838753ab85898388d8d38
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-01 13:49:56 +0300

    add a method to save  an editor. attach tags and date posted to the  articles

--- news/models.py
+++ news/models.py
@@ -7,6 +7,9 @@ class Editor(models.Model):
 
     def __str__(self):
         return self.first_name
+
+    def save_editor(self):
+        self.save()
     class Meta:
         ordering = ['first_name']
 
@@ -20,3 +23,8 @@ class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
