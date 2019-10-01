e466fcb2fd0a00656760deaa57b28dddf2c4fbaa
daniel njuguna <njugunadaniel364@gmail.com>
2019-10-01 16:16:42 +0300

    import the article model and update news_today function to call our todays  news method

--- news/views.py
+++ news/views.py
@@ -35,11 +35,14 @@ def past_days_news(request,past_date):
         assert False
 
     if date == dt.date.today():
-        return redirect(news_of_day)
+        return redirect(news_today)
 
-    return render(request, 'all-news/past-news.html', {"date": date})
+    news = Article.days_news(date)
+    return render(request, 'all-news/past-news.html',{"date": date,"news":news})
+
 
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
     return render(request, 'all-news/today-news.html', {"date": date,"news":news})
+
